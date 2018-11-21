eff81aeeffec02d78f2b7ffbb11b9be1104e9589
john <luckyfire2018@gmail.com>
2018-11-21 22:04:19 +0700

    Admin - edit bookappt

---
+++ app/src/main/java/com/project/xetnghiem/activities/DoneApptActivity.java
@@ -0,0 +1,15 @@
+package com.project.xetnghiem.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.project.xetnghiem.R;
+
+public class DoneApptActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_done_appt);
+    }
+}
